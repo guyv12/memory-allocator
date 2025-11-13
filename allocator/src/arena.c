e6deeb65083f233ce359d918aafa2cf1f35d7385
guyv12 <igorsitkobajorski@gmail.com>
2025-11-13 09:39:57 +0100

    fix(allocator) mmap arguments

--- allocator/src/arena.c
+++ allocator/src/arena.c
@@ -1,5 +1,7 @@
 #include "arena.h"
 
+#include <stdio.h>
+#include <errno.h>
 #include <sys/mman.h>
 #include <string.h>
 #include <stdint.h>
@@ -7,8 +9,9 @@
 
 int InitArena(MyArena *restrict __arena, const size_t __size)
 {
-    __arena->_mem = mmap(NULL, __size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS, -1, 0);
-    if (__arena->_mem == MAP_FAILED) return ARENA_ERROR;
+    __arena->_mem = mmap(NULL, __size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+    if (__arena->_mem == MAP_FAILED)
+        return ARENA_ERROR;
 
     __arena->_sp = __arena->_mem;
     __arena->_size = 0; __arena->_capacity = __size;
@@ -28,13 +31,14 @@ int ArenaPush(MyArena *const __arena, void *__data, size_t __size)
 {
     if (__arena->_size + __size > __arena->_capacity) 
     {
-        void *new_mapping = mmap(NULL, __arena->_capacity *= 2, PROT_READ | PROT_WRITE, MAP_ANONYMOUS, -1, 0);
+        size_t old_capacity = __arena->_capacity;
+        void *new_mapping = mmap(NULL, __arena->_capacity *= 2, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
         if (new_mapping == MAP_FAILED) return ARENA_ERROR;
-
-        munmap(__arena->_mem, __arena->_capacity); // deactivate old
 
         memcpy(new_mapping, __arena->_mem, __arena->_size);
         __arena->_mem = new_mapping; __arena->_sp = __arena->_mem;
+
+        munmap(__arena->_mem, old_capacity); // deactivate old
     }
 
     memcpy(__arena->_sp, __data, __size);
