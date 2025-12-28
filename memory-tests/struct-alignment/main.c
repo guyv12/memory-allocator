d243c2998dc12d57e07b341361c1b585ee1d1ea6
guyv12 <igorsitkobajorski@gmail.com>
2025-12-29 00:24:27 +0100

    feat(memory unalignment test): add unalignment memory test test is about access times to the aligned vs unaligned structs

---
+++ memory-tests/struct-alignment/main.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdalign.h>
+#include <time.h>
+#include <stdint.h>
+#include <math.h>
+
+
+typedef struct SomeData {
+    char a;
+    int b;
+    double c;
+}
+SomeData;
+
+
+int main(int argc, char *argv[])
+{
+    size_t size = sizeof(SomeData);
+    size_t alignment = alignof(SomeData);
+    
+    printf("struct size: %ld\n", size); 
+    printf("struct alignment: %ld\n", alignment);
+
+
+    printf("Benchmark: \n");
+    int N = pow(10, 9);
+    size_t buffer_size = (N + 1) * sizeof(SomeData);
+
+    SomeData *aligned = aligned_alloc(alignment, buffer_size);
+    SomeData *unaligned = (SomeData *)((uint8_t *)aligned_alloc(alignment, buffer_size) + 1); // shift by 1 pointer (the data read is not important)
+
+    volatile double sum1 = 0;
+    volatile double sum2 = 0;
+
+
+    //----- Aligned test ----
+
+    clock_t start = clock();
+
+    for (int i = 0; i < N; i++)
+    {
+        sum1 += aligned[i].c; // GRAB ONLY DOUBLE CUZ THAT WILL HAVE THE SHIFT GRABBING 8BYTES
+    }
+
+    clock_t end = clock();
+
+    printf("aligned access time [ms]: %lf\n", ((double)(end - start) / CLOCKS_PER_SEC) * 1000);
+
+
+    //----- Unaligned test -----
+
+    start = clock();
+
+    for (int i = 0; i < N; i++)
+    {
+        sum2 += unaligned[i].c;
+    }
+
+    end = clock();
+
+    printf("unaligned access time [ms]: %lf\n", ((double)(end - start) / CLOCKS_PER_SEC) * 1000);
+
+
+    // in my case it's around 2142ms for aligned and 2155 for unaligned so ~15ms diff
+
+    return 0;
+}
