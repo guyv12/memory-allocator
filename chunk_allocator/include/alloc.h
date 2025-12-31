fe5fd9e4e7f634ac5fcb77a18ef9e00d74b7e8ba
guyv12 <igorsitkobajorski@gmail.com>
2025-12-31 05:18:06 +0100

    feat(chunk allocator): scaffold ACTUAL chunk chunk_allocator inspired by the actual glibc implementation

--- chunk_allocator/include/alloc.h
+++ chunk_allocator/include/alloc.h
@@ -7,43 +7,122 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <pthread.h>
 
 
-//-------------- CHUNKS ----------------
+//--------- CHUNK METADATA -------------
 
-typedef struct chunk_meta_t
+typedef struct mem_chunk_t
 {
-    size_t size;
-    bool free;
-   
-    chunk_meta_t *next;
+    uint32_t _size; // all chunks will be aligned to 8 -> 3LSb's can hold data
 }
-chunk_meta_t;
+mem_chunk_t;
+
+static __always_inline void *
+payload(mem_chunk_t *__metadata) { return (void *)(__metadata + 1); } // the memory after the chunk metadata
 
 
-static chunk_meta_t *chunk_head = NULL;
-static chunk_meta_t *chunk_tail = NULL;
+typedef struct mem_chunk_free_t
+{
+    mem_chunk_t meta;
 
+    mem_chunk_free_t *fd; // add pointers to the free list
+    mem_chunk_free_t *bk;
+}
+mem_chunk_free_t;
+
+// Since we keep 3 lsbs for flags we need to ignore them here
+static __always_inline uint32_t
+size(mem_chunk_t *__metadata) { return __metadata->_size & ~7; }
+
+// Flags
+static __always_inline bool
+allocated_arena(mem_chunk_t *__metadata) { return __metadata->_size & ((uint32_t)4); }
+
+static __always_inline bool
+mmaped(mem_chunk_t *__metadata) { return __metadata->_size & ((uint32_t)2); }
+
+static __always_inline bool
+in_use(mem_chunk_t *__metadata) { return __metadata->_size & ((uint32_t)1); }
+
+
+//--------------- ARENA ----------------
+
+typedef struct mem_arena_t
+{
+    pthread_mutex_t lock;
+    
+    void *bins[];
+}
+mem_arena_t;
 
 int
-chcreate();
+init_arena(mem_arena_t *__arena);
 
 int
-chdestroy();
+destroy_arena(mem_arena_t *__arena);
+
+//----------- DYNAMIC HEAP -------------
+
+typedef struct mem_heap_dynamic_t
+{
+    mem_arena_t *ar_ptr;
+
+    size_t size;
+}
+mem_heap_dynamic_t;
+
+static __always_inline void *
+memory(mem_heap_dynamic_t *__heap) { return (void *)(__heap + 1); } // the memory after heap metadata
+
+mem_heap_dynamic_t *
+new_dynamic_heap(mem_heap_dynamic_t *const __prev_heap);
+
+int
+delete_dynamic_heap(mem_heap_dynamic_t *__heap);
+
+
+//-----------  MAIN HEAP ---------------
+
+typedef struct mem_heap_main_t
+{
+    void *base;
+    void *top;
+
+    mem_chunk_free_t *free_list_head;
+}
+mem_heap_main_t;
+
+
+//-------------- HEAP -----------------
+
+typedef union mem_heap_t
+{
+    mem_heap_dynamic_t dynamic;
+    mem_heap_main_t main;
+}
+mem_heap_t;
+
+
+
+//-------------- ALLOCS ----------------
+
+__thread mem_arena_t tl_arena;
+__thread mem_heap_t tl_heap;
+
+
+#define BRK_THRESHOLD (128 * 1024)
 
 void *
-challoc(size_t __size);
-
-void *
-aligned_challoc(size_t __alignment, size_t __size);
+tlalloc(size_t __size);
 
 void
-chfree(void *__ptr);
+tlfree(void *__ptr);
 
 
 //-------------- HELPERS ---------------
 
-static inline void *
+static __always_inline void *
 align_address(void *__addr, size_t __alignment)
 {
     return (void *)(((uintptr_t)__addr + __alignment - 1) & ~(__alignment - 1)); 
--- chunk_allocator/src/alloc.c
+++ chunk_allocator/src/alloc.c
@@ -1,56 +1,81 @@
 #include "alloc.h"
+
 #include <unistd.h>
+#include <sys/mman.h>
+#include <errno.h>
+#include <limits.h>
+#include <stdalign.h>
+#include <stddef.h>
 #include <string.h>
 
 
-int
-chcreate()
-{
-    chunk_head = sbrk(0);
-    chunk_tail = NULL;
-    
-    return 0;
-}
-
+//--------------- ARENA ----------------
 
 int
-chdestroy()
+init_arena(mem_arena_t *__arena)
 {
-    brk(chunk_head);
-    chunk_head = NULL;
-    chunk_tail = NULL;
+    pthread_mutex_init(&__arena->lock, NULL);
 
     return 0;
 }
 
-
-void *
-challoc(size_t __size)
+int
+destroy_arena(mem_arena_t *__arena)
 {
-    void *addr = sbrk(0);
+    pthread_mutex_destroy(&__arena->lock);
 
-    size_t actual_alloc_size = __size + sizeof(chunk_meta_t);
-    chunk_meta_t meta = { __size, false, NULL };
-
-
-    void *request = sbrk(actual_alloc_size);
-    if (addr == (void *) -1) return NULL;
-
-    memcpy(addr, &meta, sizeof(meta));
-
-    return addr;
+    return 0;
 }
 
 
-void *
-aligned_challoc(size_t __alignment, size_t __size)
-{
+//----------- DYNAMIC HEAP -------------
 
+mem_heap_dynamic_t *
+new_dynamic_heap(mem_heap_dynamic_t *const __prev_heap, size_t __mem_size)
+{
+    mem_heap_dynamic_t *new_heap =  mmap(NULL, sizeof(mem_heap_dynamic_t) + __mem_size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+    if (new_heap == MAP_FAILED) return NULL;
+
+    new_heap->ar_ptr = __prev_heap->ar_ptr;
+    new_heap->prev = __prev_heap;
+    new_heap->size = __mem_size;
+    return new_heap;
 }
 
+int
+delete_dynamic_heap(mem_heap_dynamic_t *__heap)
+{
+    munmap(__heap, sizeof(mem_heap_dynamic_t) + __heap->size);
+    return 0;
+}
+
+
+//-------------- ALLOCS ----------------
+
+
+void *
+tlalloc(size_t __size)
+{
+    if (__size > BRK_THRESHOLD)
+    {
+        void *chunk = mmap(NULL, sizeof(mem_chunk_t) + __size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+        if (chunk == MAP_FAILED) return NULL;
+        return payload(chunk);
+    }
+}
 
 void
-chfree(void *__ptr)
+tlfree(void *__ptr)
 {
-    ((chunk_meta_t *)__ptr)->free = true;
+    mem_chunk_t *chunk_meta = (mem_chunk_t *)(__ptr) - 1; // look back for metadata
+    
+    // we can even check for double free?
+
+    if (mmaped(chunk_meta))
+    {
+        munmap(__ptr, sizeof(mem_chunk_t) + size(chunk_meta));
+    }
+
+    // remember to change in_use bit and cast to mem_chunk_free_t with set pointers
+
 }
